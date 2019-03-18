28cccc3d0312ee62a01df69df0d3ab0ad6910f43
Max Timkovich <mtimkovich@gmail.com>
2019-03-18 10:47:58 -0700

    multiple notes and touches

--- Button.gd
+++ Button.gd
@@ -5,7 +5,7 @@ onready var pressed_img = preload('res://button_down.png')
 var pressed = false
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
-	if event.is_action('leftclick'):
+	if event is InputEventScreenTouch:
 		if event.is_pressed():
 			$Sprite.texture = pressed_img
 			pressed = true
--- Fightstick.gd
+++ Fightstick.gd
@@ -1,18 +1,71 @@
 extends Node2D
 
-var previous = 0
+var previous = [0, 0]
+var current = [0, 0]
+var top_notes = {
+	1: 'F',
+	2: 'F#',
+	3: 'E',
+	4: 'C',
+	5: 'D',
+	6: 'D#',
+	7: 'C#'
+}
+var bottom_notes = {
+	1: 'A#',
+	2: 'B',
+	3: 'A',
+	4: 'G',
+	6: 'G#'
+}
+	
+func play():
+	var pitch = 4
+	var note
+	
+	if current[0] != 0:
+		note = top_notes.get(int(current[0]))
+	elif current[1] != 0:
+		note = bottom_notes.get(int(current[1]))
+		
+	if note == null:
+		return
+		
+	if $Stick/Up.pressed:
+		pitch += 1
+	if $Stick/Down.pressed:
+		pitch -= 1
+		
+	if '#' in note:
+		note = '%s%s#' % [note[0], pitch]
+	else:
+		note = '%s%s' % [note[0], pitch]
+	
+	$Toot.stream = load('res://trumpet/%s.wav' % note)
+	$Toot.play()
+	$Toot/Timer.start()
+	
+func _on_Timer_timeout():
+	$Toot.stop()
 
-func _get_fingering(row):
-	var fingering = 0
-	for i in range(row.get_child_count()-1, -1, -1):
-		if row.get_child(i).pressed:
-			fingering += pow(2, i)
-	return fingering
+func _is_pressed():
+	return current != [0, 0]
+
+func _get_fingering():
+	var current = [0, 0]
+	var n = 0
+	for row in [$Top, $Bottom]:
+		for i in range(row.get_child_count()):
+			if row.get_child(i).pressed:
+				current[n] += pow(2, i)
+		n += 1
+	return current
 	
 func _process(delta):
-	var top = _get_fingering($Top)
-	var bottom = _get_fingering($Bottom)
-	if top != previous:
-		if top != 0:
-			print(top)
-		previous = top
+	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+		current = _get_fingering()
+		if current != previous and _is_pressed():
+			play()
+		previous = current
+	else:
+		previous = [0, 0]
